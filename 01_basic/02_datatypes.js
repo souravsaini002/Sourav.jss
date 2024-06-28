de7cc159ca66529cebe70c66ecec6e1013c6c2ba
souravsaini002 <sainisourav2002@gmail.com>
2024-06-28 10:45:19 +0000

    datatype

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+      3) // code readablity should be high
+
+console.log("sourav")
+
+
+let name ="sourav"
+let age= 18
+let isLoggedin =false
+let state;
+
+// nummber => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true/false
+// null => standlon value
+// undefined => 
+// symbol => unique  // jub hum baat karenge unique chizo k liye to symbol usee krenge
+
+
+// object
+
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
